f760626412b307fd651b9e77b75880e6d6d642e8
dallinforsyth <dallfor192@gmail.com>
2023-04-26 10:40:22 -0500

    added create method

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,20 @@
 class WorkoutsController < ApplicationController
   def index
+    @workouts = Workout.all
+    render :index
+  end
+
+  def show
     @workout = Workout.first
+    render :show
+  end
+
+  def create
+    @photo = Workout.create(
+      img: params[:img],
+      body_group: params[:body_group],
+      muscle: params[:muscle],
+    )
     render :show
   end
 end
