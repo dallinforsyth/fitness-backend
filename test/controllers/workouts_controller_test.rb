21cdeaa8bae3bfdb1e75b8092788ac48f26abf48
dallinforsyth <dallfor192@gmail.com>
2023-04-26 11:27:45 -0500

    1

--- test/controllers/workouts_controller_test.rb
+++ test/controllers/workouts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class WorkoutsControllerTest < ActionDispatch::IntegrationTest
   test "index" do
-    get "/photos.json"
+    get "/workouts.json"
     assert_response 200
 
     data = JSON.parse(response.body)
@@ -10,18 +10,18 @@ class WorkoutsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create" do
-    assert_difference "Photo.count", 1 do
-      post "/photos.json", params: { name: "lake", width: 800, height: 600 }
+    assert_difference "Workout.count", 1 do
+      post "/workouts.json", params: { img: "lake", body_group: 8, muscle: 600 }
       assert_response 200
     end
   end
 
   test "show" do
-    get "/photos/#{Photo.first.id}.json"
+    get "/workouts/#{Workout.first.id}.json"
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "width", "height", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "img", "body_group", "muscle", "created_at", "updated_at"], data.keys
   end
 
   test "update" do
