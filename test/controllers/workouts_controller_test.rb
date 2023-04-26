eb1c290a565db5dc59c51032116d38c215989ec6
dallinforsyth <dallfor192@gmail.com>
2023-04-26 11:00:17 -0500

    index test

--- test/controllers/workouts_controller_test.rb
+++ test/controllers/workouts_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class WorkoutsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/photos.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Photo.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Photo.count", 1 do
+      post "/photos.json", params: { name: "lake", width: 800, height: 600 }
+      assert_response 200
+    end
+  end
 end
