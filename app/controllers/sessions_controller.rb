93d12238ba13746cf4c867ca9d335dd3c93703de
dallinforsyth <dallfor192@gmail.com>
2023-06-26 18:52:55 -0500

    newgp

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
           exp: 24.hours.from_now.to_i, # the expiration time
         },
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
-        "HS256" # the encryption algorithm
+        "HS256" # the encryption algorithms
       )
       render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
     else
