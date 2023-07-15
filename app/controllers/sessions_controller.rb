dd68f1fc29946333d316b89082346e47145b8e66
dallinforsyth <dallfor192@gmail.com>
2023-07-15 10:57:12 -0500

    new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i, # the expiration time
+          exp: 24.hours.from_now.to_i, # the expirationa time
         },
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithms
