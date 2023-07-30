62fa81bf3fb64666aa6475bc2e784bb86dd84cf3
dallinforsyth <dallfor192@gmail.com>
2023-07-30 15:17:02 -0500

    controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,17 @@
 class GoalsController < ApplicationController
+  def create
+    goal = Goal.new(goal_params)
+
+    if goal.save
+      render json: goal, status: :created
+    else
+      render json: { errors: goal.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title, :description, :target_date)
+  end
 end
