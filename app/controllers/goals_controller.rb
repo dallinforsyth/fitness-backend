d4d532bc9882132f7451b8626b1215b0d79cd4dc
dallinforsyth <dallfor192@gmail.com>
2023-07-30 15:35:09 -0500

    work

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,6 +1,6 @@
 class GoalsController < ApplicationController
   def create
-    goal = Goal.new(goal_params)
+    goal = current_user.goals.new(goal_params)
 
     if goal.save
       render json: goal, status: :created
